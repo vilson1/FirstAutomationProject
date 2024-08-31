1773951719ceed29cfb0fd4d195a399c1fbac579
Feruze21 <hebibasiferuze89@gmail.com>
2024-08-31 23:37:40 +0200

    Homeworks

---
+++ src/test/java/review_1/ReziHomeWork/FindElementsTC1.java
@@ -0,0 +1,67 @@
+package review_1.ReziHomeWork;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+import java.util.List;
+
+public class FindElementsTC1 {
+    //TC #1: Checking the number of links on the page
+    //1. Open Chrome browser
+    //2. Go to https://www.openxcell.com
+    //3. Count the number of the links on the page and verify
+    WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.get("https://www.openxcell.com");
+    }
+
+    @Test
+    public void TC1 (){
+        List<WebElement> allLinks= driver.findElements(By.tagName("a"));
+
+        int nrofLinks= allLinks.size();
+        Assert.assertEquals(nrofLinks,332);
+
+    }
+    @Test
+    public void TC2(){
+        //TC #2: Printing out the texts of the links on the page
+        //1. Open Chrome browser
+        //2. Go to https://www.openxcell.com
+        //3. Print out all of the texts of the links on the page
+        List<WebElement> allLinks= driver.findElements(By.tagName("a"));
+        for (WebElement eachLink : allLinks) {
+            System.out.println(eachLink.getText());
+        }
+
+    }
+    @Test
+    public void TC3(){
+        //TC #3: Counting the number of links that does not have text
+        //1. Open Chrome browser
+        //2. Go to https://www.openxcell.com
+        //3. Count the number of links that does not have text and verify
+        //Expected: 109
+        List<WebElement> allLinks= driver.findElements(By.tagName("a"));
+        int linksWithoutText=0;
+        for (WebElement eachLink : allLinks) {
+            if(eachLink.getText().isEmpty()){
+                linksWithoutText++;
+            }
+            Assert.assertEquals(linksWithoutText,109);
+        }
+    }
+    @AfterClass
+    public void tearDown(){
+    driver.quit();
+    }
+
+}
---
+++ src/test/java/review_1/ReziHomeWork/SmarterBearLink.java
@@ -0,0 +1,47 @@
+package review_1.ReziHomeWork;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+import java.util.List;
+
+public class SmarterBearLink {
+    WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        //1. Open browser
+        //2. Go to website:
+        //http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+    }
+    @Test
+    public void smarterBearLinks(){
+        //3. Enter username: “Tester”
+        //4. Enter password: “test”
+        //5. Click to Login button
+        WebElement userName= driver.findElement(By.id("ctl00_MainContent_username"));
+        userName.sendKeys("Tester");
+        WebElement passWord= driver.findElement(By.id("ctl00_MainContent_password"));
+        passWord.sendKeys("test");
+        WebElement loginButton= driver.findElement(By.id("ctl00_MainContent_login_button"));
+        loginButton.click();
+       //Print out count of all the links on landing page
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        //Print out each link text on this page
+        for(WebElement eachLinktxt: links){
+            System.out.println("eachLinktxt.getText() = " + eachLinktxt.getText());
+        }
+
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
