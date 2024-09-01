979928aa385ccbf2a2f08a3f638afdceb3dc531b
User <vilson.shehu@outlook.com>
2024-09-01 14:52:03 +0200

    day 12

---
+++ src/test/java/day_12/TC1.java
@@ -0,0 +1,11 @@
+package day_12;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class TC1 {
+    @Test
+    public void TC1(){
+        Driver.getDriver().get("url");
+    }
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,39 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private Driver(){}
+
+    private static WebDriver driver;
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            String browserType = ConfigurationReader.getProperty("browser");
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        }
+        return driver;
+
+    }
+
+    public static void closeDriver(){
+        if (driver!=null)
+        driver.quit();
+    }
+}
