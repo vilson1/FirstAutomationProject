d8dd7a27caf0f642acd5b7870dec9ad786ce6452
User <vilson.shehu@outlook.com>
2024-09-05 15:54:59 +0200

    day 14

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -26,7 +26,7 @@ public class Driver {
                     break;
             }
             driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         }
         return driver;
 
